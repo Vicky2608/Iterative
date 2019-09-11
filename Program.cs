2f3108cb40e2c23d07204fe30d9bbf535131e040
Vicky2608 <vigneshk3@mail.usf.edu>
2019-09-10 20:56:36 -0400

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+
+namespace Iterative
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("How many years experience do you have in professional programming?");
+            try
+            {
+                string input = Console.ReadLine();
+                int yrs_of_exp = int.Parse(input);
+                for (int i = 0; i <= yrs_of_exp; i++)
+                {
+                    Console.WriteLine("You have got " + i.ToString() + " years of experience");
+                }
+                    Console.ReadKey(true);
+            }
+            catch
+            {
+                Console.WriteLine("Please enter a valid number");
+            }
+        }
+    }
+}
